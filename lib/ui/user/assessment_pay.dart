a15097481676b4288a0a248651631252ff26635e
wyl2022 <2373073266@qq.com>
2022-03-16 17:32:13 +0800

    更改

--- lib/ui/user/assessment_pay.dart
+++ lib/ui/user/assessment_pay.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:cloud_car/ui/notice/notice_fashionable.dart';
+import 'package:cloud_car/ui/user/pay_results.dart';
 import 'package:cloud_car/utils/headers.dart';
+import 'package:cloud_car/widget/button/cloud_back_button.dart';
 import 'package:cloud_car/widget/cloud_scaffold.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -38,7 +42,6 @@ class _AssessmentPayPageState extends State<AssessmentPayPage>
   @override
   Widget build(BuildContext context) {
     //super.build(context);
-<<<<<<< HEAD
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromRGBO(246, 246, 246, 1),
@@ -117,91 +120,57 @@ class _AssessmentPayPageState extends State<AssessmentPayPage>
     );
   }
 
-=======
-    return CloudScaffold(
-        bodyColor: bgColor,
-        appbar: Container(
-          color: Colors.transparent,
-          height: kToolbarHeight + MediaQuery.of(context).padding.top,
-          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-          child: Row(children: [
-            32.wb,
-            GestureDetector(
-              onTap: () {
-                Get.back();
-              },
-              child: Image.asset('assets/icons/back.png',
-                  height: 48.w, width: 48.w),
-            ),
-            188.wb,
-            Text(
-              '评估次数充值',
-              style: TextStyle(
-                  fontSize: 36.sp, color: const Color.fromRGBO(17, 17, 17, 1)),
-            ),
-          ]),
-        ),
-        extendBody: true,
-        //extendBodyBehindAppBar: true,
-        body: Column(
-          children: [
-            _getPay()
-            //Text('data')
-            // _getPay(),
-            // ElevatedButton(
-            //     //style: Colors.blue,
-            //     onPressed: () {},
-            //     child: const Text("立即支付")),
-          ],
-        ));
-  }
-
->>>>>>> 912f287d61642e8b5bfe0348165527b7eb9eef82
 //支付
   _getPay() {
-    return Container(
-      height: 400.w,
-      padding: const EdgeInsets.all(20),
-      child: ListView.builder(
-          itemCount: 2, //payList.length,
-          itemBuilder: (context, index) {
-            return _getPayList(payList[index]);
-            //  return
-          }),
-    );
+    return SizedBox(
+        height: 324.w,
+        //padding: EdgeInsets.only(top: 168.w),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(16.w)),
+          child: ListView.builder(
+              itemCount: 2, //payList.length,
+              itemBuilder: (context, index) {
+                return _getPayList(payList[index]);
+                //  return
+              }),
+        ));
   }
 
 //支付判断
   _getPayList(item) {
     return Column(
       children: [
-        // ListTile(
-        //   leading: Image.asset(item['src']),
-        //   title: Text(item['title']),
-        //   trailing: Radio(
-        //     value: item['checked'],
-        //     groupValue: item['checked'],
-        //     activeColor: Colors.blue,
-        //     onChanged: (value) {
-        //       setState(() {
-        //         item['checked'] = value;
-        //         // for (var i = 0; i < payList.length; i++) {
-        //         //   payList[i]['checked'] = false;
-        //         // }
-        //         // item['checked'] = true;
-        //       });
-        //     },
-        //   ),
-        //   // trailing: item['checked'] ? const Icon(Icons.check) : const Text(''),
-        //   // onTap: () {
-        //   //   setState(() {
-        //   //     for (var i = 0; i < payList.length; i++) {
-        //   //       payList[i]['checked'] = false;
-        //   //     }
-        //   //     item['checked'] = true;
-        //   //   });
-        //   // },
-        // ),
+        ListTile(
+          leading: Image.asset(item['src']),
+          title: Text(item['title']),
+          trailing: Radio(
+            //单选框的值
+            value: 1, //item['checked'],
+            //当前单选框的值
+            groupValue: item['checked'],
+            //单选框的颜色
+            activeColor: Colors.blue,
+            onChanged: (value) {
+              setState(() {
+                item['checked'] = value;
+                // for (var i = 0; i < payList.length; i++) {
+                //   payList[i]['checked'] = false;
+                // }
+                // item['checked'] = true;
+              });
+            },
+          ),
+          // trailing: item['checked'] ? const Icon(Icons.check) : const Text(''),
+          // onTap: () {
+          //   setState(() {
+          //     for (var i = 0; i < payList.length; i++) {
+          //       payList[i]['checked'] = false;
+          //     }
+          //     item['checked'] = true;
+          //   });
+          // },
+        ),
         const Divider()
       ],
     );
