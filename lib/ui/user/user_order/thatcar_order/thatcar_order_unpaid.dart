fc16013d4e8525e01122019e123a3dec1f9fef4e
wyl2022 <2373073266@qq.com>
2022-04-06 10:28:08 +0800

    我的

--- lib/ui/user/user_order/thatcar_order/thatcar_order_unpaid.dart
+++ lib/ui/user/user_order/thatcar_order/thatcar_order_unpaid.dart
@@ -3,13 +3,15 @@ import 'package:cloud_car/widget/button/cloud_back_button.dart';
 import 'package:flutter/material.dart';
 
 class ThatcarUnpaid extends StatefulWidget {
-  const ThatcarUnpaid({Key? key}) : super(key: key);
+  final String stat;
+  const ThatcarUnpaid({Key? key, required this.stat}) : super(key: key);
 
   @override
   State<ThatcarUnpaid> createState() => _ThatcarUnpaidState();
 }
 
 class _ThatcarUnpaidState extends State<ThatcarUnpaid> {
+  late String stat = widget.stat;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,14 +30,65 @@ class _ThatcarUnpaidState extends State<ThatcarUnpaid> {
       ),
       backgroundColor: BodyColor,
       body: ListView(
-        children: [],
+        children: const [],
       ),
     );
   }
 
-  // getCarInformation() {
-  //   return Container(
-  //     child: Text("data"),
-  //   );
-  // }
+  getCarBox(Widget widget) {
+    return Container(
+      padding: EdgeInsets.all(16.w),
+      margin: EdgeInsets.symmetric(horizontal: 32.w, vertical: 16.w),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.w)),
+      child: widget,
+    );
+  }
+
+  _getCarInformation(String stat) {
+    return getCarBox(Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              '叫车车辆信息',
+              style: Theme.of(context).textTheme.subtitle2,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.w),
+              decoration: BoxDecoration(
+                color: const Color(0xFFFE8029).withOpacity(0.01),
+              ),
+              child: Text(
+                stat,
+                style: TextStyle(
+                    fontSize: BaseStyle.fontSize24,
+                    color: const Color(0xFFFE8029)),
+              ),
+            )
+          ],
+        ),
+        24.hb,
+        Row(
+          children: [
+            SizedBox(
+              width: 196.w,
+              height: 150.w,
+              child: Image.asset(
+                Assets.images.carBanner.path,
+                fit: BoxFit.fill,
+              ),
+            )
+          ],
+        ),
+        Column(
+          children: [
+            Text(
+              '奥迪Q3 2020款 35 TFSI 进取型SUV',
+              style: TextStyle(color: BaseStyle.color111111),
+            )
+          ],
+        )
+      ],
+    ));
+  }
 }
--- lib/widget/thatcar_widget.dart
+++ lib/widget/thatcar_widget.dart
@@ -4,24 +4,46 @@ import 'package:flutter/material.dart';
 abstract class ThatcarWidget extends StatefulWidget {
   final String title;
   final String text;
-  const ThatcarWidget({Key? key, required this.title, required this.text})
+  final String stat;
+  const ThatcarWidget(
+      {Key? key, required this.title, required this.text, required this.stat})
       : super(key: key);
 
-  @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16.w),
       margin: EdgeInsets.symmetric(horizontal: 32.w, vertical: 32.w),
     );
-
-
   }
 
   getThatcar() {
     return Column(
-      children: [title=="叫车车辆信息"?Row(children: [Text(title,style:TextStyle(color: BaseStyle.color333333,fontSize: BaseStyle.fontSize28) ,)],)],
+      children: [
+        title == "叫车车辆信息"
+            ? Row(
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                        color: BaseStyle.color333333,
+                        fontSize: BaseStyle.fontSize28),
+                  ),
+                  Container(
+                    width: 104.w,
+                    height: 40.w,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.w),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4.w),
+                        color: stat.substring(0, 1) == "待"
+                            ? const Color(0xFFFE8029).withOpacity(0.01)
+                            : const Color(0xFF027AFF).withOpacity(0.01)),
+                    child: Text(stat),
+                  )
+                ],
+              )
+            : const SizedBox()
+      ],
     );
   }
 }
-
-
