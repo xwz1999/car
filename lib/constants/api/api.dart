9c89183f9ccdab2cc81855fc3b0101c8fe3081d7
张萌 <494089941@qq.com>
2022-04-21 14:20:14 +0800

    修改api

--- lib/constants/api/api.dart
+++ lib/constants/api/api.dart
@@ -1,9 +1,8 @@
-
 import '../environment/environment.dart';
+
 part 'user_api.dart';
 
 part 'home_api.dart';
-
 
 /// * user  用户接口
 /// * data 基础数据接口
@@ -29,18 +28,17 @@ class API {
   }
 
   static String get baseURL => '$host/app/broker';
+
   //根分类
   static _File file = _File();
   static _Login login = _Login();
 
-
   static _Storemanagement storemanagement = _Storemanagement();
   static _Order order = _Order();
+
   //二级分类
   static _UserApi user = _UserApi();
   static _HomeApi home = _HomeApi();
-
-
 }
 
 extension ImageOnString on String {
@@ -67,29 +65,37 @@ class _Login {
   String get phone => '/login/phone';
 
   ///微信登录
-  String get weixin => 'login/wechat';
+  String get weixin => '/login/wechat';
 
   ///微信登录绑定账号
-  String get weixinBind => 'login/bind';
+  String get weixinBind => '/login/bind';
 }
 
 class _Order {
   //寄卖订单->寄卖订单列表
   get consignmentLists => '/order/consignment/lists';
+
 //寄卖订单->发布车辆
   get consignmentPublish => '/order/consignment/publish';
+
 //邀约
   get inviteAdd => '/order/invite/add';
+
 //售车订单->售车订单列表
   get saleLists => '/order/sale/lists';
+
 //上传检车报告
   get saleTestrepord => '/order/sale/test_report';
+
 //过户
   get scaleTransfer => '/order/sale/transfer';
+
 //完结订单
   get saleFinal => '/order/sale/final';
+
 //叫车订单->添加叫车订单
   get carAdd => '/order/call/car/add';
+
 //交车
   get carFinal => '/order/call/car/final';
 }
@@ -98,12 +104,16 @@ class _Order {
 class _Storemanagement {
 //组织架构
   get storeAll => '/business/store/all';
+
   //全部角色
   get roleAll => '/business/role/all';
+
   //门店筛选
   get storeSelect => '/business/store/select';
+
   //添加门店
   get storeAdd => '/business/store/add';
+
   //添加员工
   get staffAdd => '/business/staff/add';
 }
