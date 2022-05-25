4bd4484ba04773866c95dcbbf9d1231d33d7b619
wyl2022 <2373073266@qq.com>
2022-05-25 17:03:23 +0800

    no message

---
+++ lib/model/user/staffinfo_model.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'staffinfo_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+StaffinfoModel _$StaffinfoModelFromJson(Map<String, dynamic> json) =>
+    StaffinfoModel(
+      Id: json['Id'] as int,
+      Name: json['Name'] as String,
+      Gender: json['Gender'] as int,
+      Phone: json['Phone'] as String,
+      StoreId: json['StoreId'] as int,
+      StoreName: json['StoreName'] as String,
+      RoleId: json['RoleId'] as int,
+      RoleName: json['RoleName'] as String,
+      Commission: json['Commission'] as String,
+      Status: json['Status'] as int,
+      AuditStatus: json['AuditStatus'] as int,
+    );
