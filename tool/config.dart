b698a1fa4d2b11794a45314eaa7bb0f7f94ca422
张萌 <494089941@qq.com>
2022-05-26 15:00:05 +0800

    打包配置文件 添加 更改路径为相对路径

---
+++ tool/config.dart
@@ -0,0 +1,17 @@
+class Config {
+  ///用户根目录
+  static const String homeDir = '/users/zhangmeng';
+
+  ///包名
+  static const String packageName = 'cloud_car';
+
+  ///打包目录
+  static String get buildPath =>
+      './build/app/outputs/flutter-apk/app-release.apk';
+
+  ///测试包文件夹
+  static String get apkDevDir => '../team/cloud_car/dev';
+
+  ///正式包文件夹
+  static String get apkDir => '../team/cloud_car/release';
+}
